afaabf418e4b4451793db9e2c7c3183d0888229e
youchunguang <youchunguang@dxxzpt.com>
2020-12-15 11:39:44 +0800

    统一返回的数据格式

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,14 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * @param null $message
+     * @param array $data
+     * @param bool $status
+     * @param int $code
+     * @return array
+     */
+    public function returnMessage($message=null,$data=[],$status=true,$code=400200){
+        return ['status'=>$status,'message'=>$message,'data'=>$data,'code'=>$code];
+    }
 }

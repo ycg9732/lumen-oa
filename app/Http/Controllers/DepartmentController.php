3e3a96daff3d58127004df9b97d0115d1108b2f5
youchunguang <youchunguang@dxxzpt.com>
2020-12-15 10:55:18 +0800

    员工、组织架构

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,10 +4,62 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\department;
+use Illuminate\Http\Request;
+
 class DepartmentController extends Controller
 {
     /**
-     *
+     * 搜索接口(名称搜索)
      */
+    public function de_search(Request $request){
+        $name = $request->input('de_name');
+        $de_list = department::where('dept_name','like','%'.$name.'%')->get();
+        return $de_list;
+    }
+    /**
+     *组织架构列表接口
+     */
+    public function de_list(){
+        //todo 分页
+        $de_list = department::all();
+        return $de_list;
+    }
+    /**
+     * 组织架构详情
+     */
+    public function de_detil(Request $request){
+        $de_id = $request->input('de_id');
+        $de_info = department::where('id',$de_id)->get();
+        return $de_info;
+    }
+    /**
+     * 添加组织架构
+     */
+    public function de_add(Request $request){
+        $name = department::where('dept_name',$request->input('de_name'))->count();
+        if ($name > 0){
+            return 'department exist';
+        }else{
+            $department = new department;
+            $department->dept_name = $request->input('de_name');
+//            $department->co_code = $request->input('co_code');
+            $department->save();
+            //todo
+            return 'ok';
+        }
+    }
+    /**
+     *修改详情
+     */
+    public function de_edit(Request $request){
+        $de_id = $request->input('de_id');
+        $department = department::find($de_id);
+        $department->dept_name = $request->input('de_name');
+//        $department->co_code = $request->input('co_code');
+        $department->save();
+        //todo
+        return 'ok';
+    }
 
 }
--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,7 +4,61 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\employee;
+use Illuminate\Http\Request;
+
 class EmployeeController extends Controller
 {
-
+    /**
+     * 搜索接口(名称搜索)
+     */
+    public function ee_search(Request $request){
+        $name = $request->input('ee_name');
+        $ee_list = employee::where('ee_name','like','%'.$name.'%')->get();
+        return $ee_list;
+    }
+    /**
+     *员工列表接口
+     */
+    public function ee_list(){
+        //todo 分页
+        $ee_list = employee::all();
+        return $ee_list;
+    }
+    /**
+     * 员工详情
+     */
+    public function ee_detil(Request $request){
+        $ee_id = $request->input('ee_id');
+        $ee_info = employee::where('id',$ee_id)->get();
+        return $ee_info;
+    }
+    /**
+     * 添加员工
+     */
+    public function ee_add(Request $request){
+        $id_card = employee::where('id_card',$request->input('id_card'))->count();
+        if ($id_card > 0){
+            return 'employee exist';
+        }else{
+            $employee = new employee;
+            $employee->ee_name = $request->input('ee_name');
+            $employee->id_card = $request->input('id_card');
+            $employee->save();
+            //todo
+            return 'ok';
+        }
+    }
+    /**
+     *修改详情
+     */
+    public function ee_edit(Request $request){
+        $ee_id = $request->input('ee_id');
+        $employee = employee::find($ee_id);
+        $employee->ee_name = $request->input('ee_name');
+        $employee->id_card = $request->input('id_card');
+        $employee->save();
+        //todo
+        return 'ok';
+    }
 }

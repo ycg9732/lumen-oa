4a0b9c1a1072dd6d83d8ab30653fa822c80390cf
youchunguang <youchunguang@dxxzpt.com>
2021-01-05 14:38:31 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -116,13 +116,13 @@ $router->get('/permission_add','RoleController@permission_add');
 $router->get('/con_company','ContractController@con_company');
 //供应商图片上传
 $router->post('/supplier_img',['middleware'=>'cors',function() use($router){
-//        $router->post('/supplier_img','FileController@supplier_img');
-        return 111;
-}]);
+        $router->post('/supplier_img','FileController@supplier_img');
+     }]);
 //\Illuminate\Support\Facades\Route::middleware(['cors'])->group(function (){
 //    \Illuminate\Support\Facades\Route::post('/supplier_img','FileController@supplier_img');
 //});
-$router->post('/supplier_img','FileController@supplier_img');
+\Illuminate\Support\Facades\Route::post('/supplier_img','FileController@supplier_img')->middleware(['cors']);
+//$router->post('/supplier_img','FileController@supplier_img');
 //excel导出测试
 $router->get('/excel','FileController@excel');
 //供应商添加

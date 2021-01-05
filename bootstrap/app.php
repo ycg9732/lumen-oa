4f5446d0ae5d7cddc2e30f1b3869731033522d3a
youchunguang <youchunguang@dxxzpt.com>
2021-01-05 13:02:42 +0800

    路由分配中间件

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -74,13 +74,13 @@ $app->configure('app');
 
  $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
-     App\Http\Middleware\CorsMiddleware::class
+//     App\Http\Middleware\CorsMiddleware::class
 
  ]);
 
  $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
-//        'cors' => App\Http\Middleware\ResumableMiddleware::class,
+        'cors' => App\Http\Middleware\CorsMiddleware::class,
  ]);
 
 /*

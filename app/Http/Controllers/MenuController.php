c167b84be9e756510470b408410271f201c6306c
youchunguang <youchunguang@dxxzpt.com>
2021-01-06 14:56:11 +0800

    删除菜单

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -9,14 +9,14 @@ class MenuController extends Controller
     public function menu(){
         $res = [
             ['title'=>'企业管理','path'=>'/company','icon'=>'dashboard'],
-            ['title'=>'财务管理','path'=>'/finance','icon'=>'dashboard'],
+//            ['title'=>'财务管理','path'=>'/finance','icon'=>'dashboard'],
             ['title'=>'人事管理','path'=>'/personnel','icon'=>'dashboard'],
-            ['title'=>'仓储管理','path'=>'/storage','icon'=>'dashboard'],
-            ['title'=>'物流管理','path'=>'/distribution','icon'=>'dashboard'],
-            ['title'=>'追踪管理','path'=>'/trace','icon'=>'dashboard'],
-            ['title'=>'资料管理','path'=>'/resource','icon'=>'dashboard'],
-            ['title'=>'采购管理','path'=>'/purchase','icon'=>'dashboard'],
-            ['title'=>'销售管理','path'=>'/marker','icon'=>'dashboard'],
+//            ['title'=>'仓储管理','path'=>'/storage','icon'=>'dashboard'],
+//            ['title'=>'物流管理','path'=>'/distribution','icon'=>'dashboard'],
+//            ['title'=>'追踪管理','path'=>'/trace','icon'=>'dashboard'],
+//            ['title'=>'资料管理','path'=>'/resource','icon'=>'dashboard'],
+//            ['title'=>'采购管理','path'=>'/purchase','icon'=>'dashboard'],
+//            ['title'=>'销售管理','path'=>'/marker','icon'=>'dashboard'],
         ];
         return $this->returnMessage($res);
     }

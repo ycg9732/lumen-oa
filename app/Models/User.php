a4ce394e98ab9a256636da63a2bacbeb57c7a4fc
youchunguang <youchunguang@dxxzpt.com>
2021-01-15 15:28:33 +0800

    判断用户是否有特定权限

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,5 +39,35 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     public function role(){
         return $this->belongsToMany(role::class,'user_role','user_id','role_id');
     }
-
+    /**
+     * by you
+     * @param $p_name|权限的名称
+     * 判断用户是否拥有该权限
+     */
+    public function has_permission($p_name)
+    {
+        $user_role = $this->role()->get();
+        if (empty($user_role)){
+            return '该用户没有分配角色';
+        }
+        $p_id = permission::where('p_name',$p_name)->value('id');
+        if (empty($p_id)){
+            return '系统没有该权限';
+           }
+        $permission = permission::find($p_id);
+        $p_de_r = $permission->role()->get();
+        $u_role_id = [];
+        foreach ($user_role as $k1 => $v1){
+            $u_role_id[] = $v1['id'];
+        }
+        $p_role_id = [];
+        foreach ($p_de_r as $k2 => $v2){
+            $p_role_id[] = $v2['id'];
+        }
+        if (empty(array_intersect($u_role_id,$p_role_id))){
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
